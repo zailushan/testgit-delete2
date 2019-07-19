ce9135a9abc243dc65ecdf6b904c46f96412a63d
caowl <caowl@tritonsfs.com>
2019-07-19 14:41:59 +0800

    test1

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @author caowl
+ * @description: TODO
+ * @date 2019/7/19 14:26
+ */
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+    }
+
+}
